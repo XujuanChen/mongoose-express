d872d15531274377cd8ef5a6e52f371c86423eb3
XujuanChen <xchen022@citymail.cuny.edu>
2022-07-26 16:57:47 -0400

    update

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const path = require ('path');
 const app = express();
 const Product = require('./models/Product');
+const methodOverride = require('method-override');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'));
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/farmStand')
@@ -38,6 +40,18 @@ app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
     res.render('products/show', { product })
+})
+
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit', { product })
+})
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true});
+    res.redirect(`/products/${product._id}`)
 })
 
 app.listen(3000, () => {
