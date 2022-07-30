21c8d77e3729a114c6ce7861d0596b1636d3c352
XujuanChen <xchen022@citymail.cuny.edu>
2022-07-29 21:47:03 -0400

    filter

--- index.js
+++ index.js
@@ -20,10 +20,17 @@ app.get('/', (req, res) => {
 })
 
 app.get('/products', async (req, res) => {
-    const products = await Product.find({});
     // console.log(products);
     // res.send("PRODUCT PAGE")
-    res.render('products/index', { products });
+    const { category } = req.query;
+    if ( category ) {
+        const products = await Product.find( { category });
+        res.render('products/index', { products, category })
+    } else {
+        const products = await Product.find( {});
+        res.render('products/index', { products, category: 'All' })
+    }
+
 })
 
 app.get('/products/new', (req, res) => {
